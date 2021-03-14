e0daf082fcc23833a277da649b49611a8d2865e3
Estebanm1812 <69942961+Estebanm1812@users.noreply.github.com>
2021-03-14 13:58:27 -0500

    Aplication starts correctly, not funtions yet

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,54 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.Restaurant;
 
-}
+public class Main extends Application{
+
+	private Restaurant restaurant;
+	private RestaurantGUI restaurantGUI;
+	
+	public Main() {
+		
+		restaurant = new Restaurant();
+		restaurantGUI = new RestaurantGUI(restaurant);
+		
+	}
+	public static void main(String[]args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("pane.fxml"));
+		FXMLLoader fxmlLoader2 = new FXMLLoader(getClass().getResource("LoginPane.fxml"));
+		
+		fxmlLoader.setController(restaurantGUI);
+		
+		fxmlLoader2.setController(restaurantGUI);
+	    Parent loginPane = fxmlLoader.load();
+		
+		Parent root2 = fxmlLoader2.load();
+		
+		
+		Scene scene2 = new Scene(root2);
+		
+		primaryStage.setTitle("The Golden House");
+		primaryStage.setScene(scene2);
+		primaryStage.show();
+		
+		
+		
+	}
+
+
+		
+	}
+
+	
+	
+	
--- src/ui/RestaurantGUI.java
+++ src/ui/RestaurantGUI.java
@@ -1,5 +1,32 @@
 package ui;
 
-public class RestaurantGUI {
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import model.Restaurant;
 
+public class RestaurantGUI {
+	
+
+    @FXML
+    private BorderPane firstPane;
+	
+    @FXML
+    private BorderPane pane;
+    
+	private Restaurant restaurant;
+	
+	public RestaurantGUI(Restaurant restaurant) {
+	
+		this.restaurant = restaurant;
+		
+		
+	}
+	public BorderPane getPane() {
+		return pane;
+	}
+	public void SetPane(BorderPane pane) {
+		this.pane = pane;
+	}
+	
+	
 }
