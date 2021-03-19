37aee3a430109594b631ac072e7113e122ee7ce8
Estebanm1812 <69942961+Estebanm1812@users.noreply.github.com>
2021-03-18 21:23:35 -0500

    most of persistence methods added

--- src/model/Restaurant.java
+++ src/model/Restaurant.java
@@ -1,8 +1,13 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,7 +30,7 @@ public class Restaurant {
 	
 	private List<User> userList;
 	
-	
+	private List<Delivery> deliveriesList;
 	
 	
 	public Restaurant() {
@@ -37,6 +42,8 @@ public class Restaurant {
 		setIngredientsList(new ArrayList<Ingredient>());
 		
 		userList = new ArrayList<User>();
+		
+		deliveriesList = new ArrayList<Delivery>();
 		
 	}
 	public void addCustomer(String name, String lastName, String id, String addres, String phone, String comment) {
@@ -146,6 +153,89 @@ public class Restaurant {
 		oos.writeObject(userList);
 		oos.close();
 	}
+	public void saveDeliversData() throws FileNotFoundException, IOException {
+		
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(DELIVERS_SAVE_PATH_FILE));
+		
+		oos.writeObject(deliveriesList);
+		oos.close();
+	}
+	public void importEmployee(String fileName) throws IOException{
+		    BufferedReader br = new BufferedReader(new FileReader(fileName));
+		    String line = br.readLine();
+		    while(line!=null){
+		      String[] parts = line.split(";");
+		      addEmployee(parts[0],parts[1],parts[2]);
+		      line = br.readLine();
+		    }
+		    br.close();
+		  }
+	public void importDelivery(String fileName) throws IOException{
+	    BufferedReader br = new BufferedReader(new FileReader(fileName));
+	    String line = br.readLine();
+	    while(line!=null){
+	      String[] parts = line.split(";");
+	      addDelivery(parts[0],parts[1],parts[2]);
+	      line = br.readLine();
+	    }
+	    br.close();
+	  }
+	private void addDelivery(String string, String string2, String string3) {
 	
+		
+	}
+	public void importUsers(String fileName) throws IOException{
+	    BufferedReader br = new BufferedReader(new FileReader(fileName));
+	    String line = br.readLine();
+	    while(line!=null){
+	      String[] parts = line.split(";");
+	      addUser(parts[0],parts[1],parts[2], parts[3], parts[4]);
+	      line = br.readLine();
+	    }
+	    br.close();
+	  }
+	public void importCustomers(String fileName) throws IOException{
+	    BufferedReader br = new BufferedReader(new FileReader(fileName));
+	    String line = br.readLine();
+	    while(line!=null){
+	      String[] parts = line.split(";");
+	      addCustomer(parts[0],parts[1],parts[2], parts[3], parts[4], parts[5]);
+	      line = br.readLine();
+	    }
+	    br.close();
+	  }
+
+	public void loadEmployeeData() throws IOException, ClassNotFoundException{
+	    File f = new File(EMPLOYEERS_SAVE_PATH_FILE);
+	    if(f.exists()){
+	      ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+	      employersList = (List<Employee>)ois.readObject();
+	      ois.close();
+	    }
+	  }
+	public void loadUserData() throws IOException, ClassNotFoundException{
+	    File f = new File(USERS_SAVE_PATH_FILE);
+	    if(f.exists()){
+	      ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+	      userList = (List<User>)ois.readObject();
+	      ois.close();
+	    }
+	  }
+	public void loadDeliveryData() throws IOException, ClassNotFoundException{
+	    File f = new File(DELIVERS_SAVE_PATH_FILE);
+	    if(f.exists()){
+	      ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+	      deliveriesList = (List<Delivery>)ois.readObject();
+	      ois.close();
+	    }
+	  }
+	public void loadCustomersData() throws IOException, ClassNotFoundException{
+	    File f = new File(CUSTOMERS_SAVE_PATH_FILE);
+	    if(f.exists()){
+	      ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+	      customersList = (List<Customer>)ois.readObject();
+	      ois.close();
+	    }
+	  }
 	
 }
